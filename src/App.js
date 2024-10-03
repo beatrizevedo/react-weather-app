15d85d86da25ed1005c80332e5d49f1c6ef27c85
Beatriz Azevedo <bia.azevedo@live.com.pt>
2024-10-03 10:19:01 +0100

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Weather App</h1>
-      </header>
-      <footer>
-        This project was coded by{' '}
-        <a href="https://github.com/beatrizevedo" target="_blank">
-          Beatriz Azevedo
-        </a>
-        . It's open-sourced on{' '}
-        <a
-          href="https://github.com/beatrizevedo/react-weather-app"
-          target="_blank"
-        >
-          Github
-        </a>
-        , hosted on{' '}
-        <a
-          href="https://beatriz-weather-app-react.netlify.app/"
-          target="_blank"
-        >
-          Netlify
-        </a>
-        .
-      </footer>
+      <div className="container">
+        <header className="App-header">
+          <h1>Weather App</h1>
+        </header>
+        <footer>
+          This project was coded by{' '}
+          <a href="https://github.com/beatrizevedo" target="_blank">
+            Beatriz Azevedo
+          </a>
+          . It's open-sourced on{' '}
+          <a
+            href="https://github.com/beatrizevedo/react-weather-app"
+            target="_blank"
+          >
+            Github
+          </a>
+          , hosted on{' '}
+          <a
+            href="https://beatriz-weather-app-react.netlify.app/"
+            target="_blank"
+          >
+            Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
