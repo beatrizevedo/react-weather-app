1e7f4f86f2290c6b487f95ad2839680238b84025
Beatriz Azevedo <bia.azevedo@live.com.pt>
2024-10-03 11:36:08 +0100

    HTML structure + CSS

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
+import Weather from './Weather';
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header">
-          <h1>Weather App</h1>
-        </header>
+        <Weather />
         <footer>
           This project was coded by{' '}
           <a href="https://github.com/beatrizevedo" target="_blank">
---
+++ src/Weather.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Weather.css';
+import icon from './img/icon.png';
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
+        <input type="search" placeholder="Enter your city" />
+        <input type="submit" value="Search" />
+      </form>
+      <img src={icon} alt="weather-icon" />
+      <h1>Porto</h1>
+      <h2>Sunday</h2>
+      <h3>29ºC</h3>
+      <h4>Sunny</h4>
+    </div>
+  );
+}
